f0b4a9d041a025546be63a0d450edd1c3f7b2e56
PrimaSentana <88036062+PrimaSentana@users.noreply.github.com>
2022-05-07 21:15:50 +0800

    Update prima.cpp

--- prima.cpp
+++ prima.cpp
@@ -1,33 +1,130 @@
 #include <iostream>
 using namespace std;
 
+int proses(int harga_barang, int jumlah) {
+	return harga_barang * jumlah;
+	
+}
 
-int main()
+int main() 
 {
-	int a = 2;
-	int b = 2;
+	string kode_barang;
+	double diskon1 = 0.5;
+	double diskon2 = 0.3;
+	int jumlah;
+	cout << "Masukkan Kode Barang : "; cin >> kode_barang;
 
-	// Boolean >>> True or false (1, 0)
-	bool hasil1, hasil2;
+	if (kode_barang == "b1" or kode_barang == "B1") {
+		string nama_barang = "Kaos Polos\n";
+		float harga_barang = 400000;
+		cout << "Nama Barang : " << nama_barang;
+		cout << "Harga Barang : " << harga_barang << endl;
 
-	// Sebanding ==
-	hasil1 = (a == b);
-	// tidak sebanding !=
-	hasil2 = (a != b);
+		cout << "Jumlah Beli : "; cin >> jumlah;
+		cout << "Total : " << proses(harga_barang, jumlah) << endl;
+		
 
-	// kurang dari
-	hasil1 = (a < b);
-	// lebih dari
-	hasil2 = (a > b);
+		if (proses(harga_barang, jumlah) >= 1000000) {
+			float tot_bayar = proses(harga_barang, jumlah) - (proses(harga_barang, jumlah) * diskon1);
+			float potongan = proses(harga_barang, jumlah) * diskon1;
+			cout << "Potongan : " << potongan;
+			cout << "Total Bayar : " << tot_bayar;
+		}
+		else if (proses(harga_barang, jumlah) >= 500000) {
+			float tot_bayar = proses(harga_barang, jumlah) - (proses(harga_barang, jumlah) * diskon2);
+			float potongan = proses(harga_barang, jumlah) * diskon2;
+			cout << "Potongan : " << potongan;
+			cout << "Total Bayar : " << tot_bayar;
+		}
+		else {
+			cout << proses(harga_barang, jumlah);
+		}
 
-	// Kurang dari sama dengan 
-	hasil1 = (a <= b);
-	// Lebih Dari sama dengan
-	hasil2 = (a >= b);
+	} 
+	if (kode_barang == "b2" or kode_barang == "B2") {
+		string nama_barang = "Celana Jeans\n";
+		float harga_barang = 450000;
+		cout << "Nama Barang : " << nama_barang;
+		cout << "Harga Barang : " << harga_barang << endl;
 
-	cout << hasil1 << endl;
-	cout << hasil2 << endl;	
+		cout << "Jumlah Beli : "; cin >> jumlah;
+		cout << "Total : " << proses(harga_barang, jumlah) << endl;
 
-	cin.get();
+
+		if (proses(harga_barang, jumlah) >= 1000000) {
+			float tot_bayar = proses(harga_barang, jumlah) - (proses(harga_barang, jumlah) * diskon1);
+			float potongan = proses(harga_barang, jumlah) * diskon1;
+			cout << "Potongan : " << potongan;
+			cout << "Total Bayar : " << tot_bayar;
+		}
+		else if (proses(harga_barang, jumlah) >= 500000) {
+			float tot_bayar = proses(harga_barang, jumlah) - (proses(harga_barang, jumlah) * diskon2);
+			float potongan = proses(harga_barang, jumlah) * diskon2;
+			cout << "Potongan : " << potongan;
+			cout << "Total Bayar : " << tot_bayar;
+		}
+		else {
+			cout << proses(harga_barang, jumlah);
+		}
+
+	}
+	if (kode_barang == "b3" or kode_barang == "B3") {
+		string nama_barang = "Kemeja Lengan Panjang\n";
+		float harga_barang = 250000;
+		cout << "Nama Barang : " << nama_barang;
+		cout << "Harga Barang : " << harga_barang << endl;
+
+		cout << "Jumlah Beli : "; cin >> jumlah;
+		cout << "Total : " << proses(harga_barang, jumlah) << endl;
+
+
+		if (proses(harga_barang, jumlah) >= 1000000) {
+			float tot_bayar = proses(harga_barang, jumlah) - (proses(harga_barang, jumlah) * diskon1);
+			float potongan = proses(harga_barang, jumlah) * diskon1;
+			cout << "Potongan : " << potongan;
+			cout << "Total Bayar : " << tot_bayar;
+		}
+		else if (proses(harga_barang, jumlah) >= 500000) {
+			float tot_bayar = proses(harga_barang, jumlah) - (proses(harga_barang, jumlah) * diskon2);
+			float potongan = proses(harga_barang, jumlah) * diskon2;
+			cout << "Potongan : " << potongan;
+			cout << "Total Bayar : " << tot_bayar;
+		}
+		else {
+			cout << proses(harga_barang, jumlah);
+		}
+
+	}
+	if (kode_barang == "b4" or kode_barang == "B4") {
+		string nama_barang = "Jaket Nasa\n";
+		float harga_barang = 525000;
+		cout << "Nama Barang : " << nama_barang;
+		cout << "Harga Barang : " << harga_barang << endl;
+
+		cout << "Jumlah Beli : "; cin >> jumlah;
+		cout << "Total : " << proses(harga_barang, jumlah) << endl;
+
+
+		if (proses(harga_barang, jumlah) >= 1000000) {
+			float tot_bayar = proses(harga_barang, jumlah) - (proses(harga_barang, jumlah) * diskon1);
+			float potongan = proses(harga_barang, jumlah) * diskon1;
+			cout << "Potongan : " << potongan;
+			cout << "Total Bayar : " << tot_bayar;
+		}
+		else if (proses(harga_barang, jumlah) >= 500000) {
+			float tot_bayar = proses(harga_barang, jumlah) - (proses(harga_barang, jumlah) * diskon2);
+			float potongan = proses(harga_barang, jumlah) * diskon2;
+			cout << "Potongan : " << potongan;
+			cout << "Total Bayar : " << tot_bayar;
+		}
+		else {
+			cout << proses(harga_barang, jumlah);
+		}
+
+	}
+	else {
+		cout << "kode barang salah";
+	}
+
 	return 0;
 }
